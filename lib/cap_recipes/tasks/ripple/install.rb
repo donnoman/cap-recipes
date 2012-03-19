cbebcf540df4983430b383dc1af60805fdd57348
Donovan Bray <donovan@homerun.com>
2012-03-19 16:23:05 -0700

    ripple now supports passing an array of hosts and round-robin'ing them.

--- lib/cap_recipes/tasks/ripple/install.rb
+++ lib/cap_recipes/tasks/ripple/install.rb
@@ -3,7 +3,9 @@ Capistrano::Configuration.instance(true).load do
 
   namespace :ripple do
     set :ripple_template_path, File.join(File.dirname(__FILE__),'ripple.yml.template')
-    set :ripple_host, "127.0.0.1"
+    set :ripple_host, "127.0.0.1" #can be an array
+    set :ripple_http_port, '8098'
+    set :ripple_pb_port, '8087'
 
     desc "Configure the Ripple Template"
     task :configure, :except => {:no_release => true} do
