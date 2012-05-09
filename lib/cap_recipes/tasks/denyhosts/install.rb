9b146c2636bd67b69e0ec7a6ec61b07ed35bf011
Donovan Bray <donovan@homerun.com>
2012-05-08 19:26:38 -0700

    allow selective override of denyhosts

--- lib/cap_recipes/tasks/denyhosts/install.rb
+++ lib/cap_recipes/tasks/denyhosts/install.rb
@@ -1,25 +1,24 @@
 Capistrano::Configuration.instance(true).load do
 
   namespace :denyhosts do
-
     set :denyhosts_root, "/var/lib/denyhosts"
     set(:denyhosts_allowed_hosts_file) {"#{denyhosts_root}/allowed-hosts"}
     set :denyhosts_allowed_hosts, %w()
 
     desc "Install Monit"
-    task :install do
+    task :install, :except => {:no_denyhosts => true} do
       utilities.apt_install "denyhosts"
     end
 
     desc "Install denyhost settings"
-    task :setup do
+    task :setup, :except => {:no_denyhosts => true} do
       put denyhosts_allowed_hosts.join("\n")+"\n", "/tmp/denyhosts_allowed_hosts"
       run "#{sudo} mv /tmp/denyhosts_allowed_hosts #{denyhosts_allowed_hosts_file}"
     end
 
     %w(start stop restart).each do |t|
       desc "#{t} denyhosts"
-      task t.to_sym do
+      task t.to_sym, :except => {:no_denyhosts => true} do
         sudo "/etc/init.d/denyhosts #{t}"
       end
     end
