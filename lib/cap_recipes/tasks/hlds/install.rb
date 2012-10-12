e11951ee2b0cae4fb20966c403a87d377a569781
Rick Russell <oipunx@gmail.com>
2012-10-12 14:38:28 -0700

    paramaterized a bit more of the init script for customizing

--- lib/cap_recipes/tasks/hlds/install.rb
+++ lib/cap_recipes/tasks/hlds/install.rb
@@ -9,20 +9,26 @@ Capistrano::Configuration.instance(true).load do
     set(:hlds_source) {hlds_root}
     set :hlds_user, "hlds"
     set :hlds_update_tool_url, "http://storefront.steampowered.com/download/hldsupdatetool.bin"
-    set :hlds_game, "tf"
     set(:hlds_bindir) { "#{hlds_root}/orangebox"}
     set :hlds_init_erb, File.join(File.dirname(__FILE__),'hlds.init.erb')
     set :hlds_init_dest, '/etc/init.d/hlds'
     set :hlds_config_erb, File.join(File.dirname(__FILE__),'server.cfg.erb')
     set :hlds_motd_txt_erb, File.join(File.dirname(__FILE__),'motd.txt.erb')
     set :hlds_motd_text_txt_erb, File.join(File.dirname(__FILE__),'motd_text.txt.erb')
+    set :ugc_files, File.join(File.dirname(__FILE__),'ugc.tar')
     set(:hlds_config_root) {"#{hlds_root}/orangebox/tf/cfg"}
     set(:hlds_config_server_cfg) {"#{hlds_config_root}/server.cfg"}
-    set :hlds_mapcycle, %w(pl_sweetwater_ugc)
-    set(:hlds_parameters) {"-autoupdate -maxplayers 18 +map #{hlds_mapcycle.first}"}
+    set(:hlds_parameters) {"-autoupdate +maxplayers #{maxplayers} +ip #{server_ip} -port #{server_port} +exec #{server_type_cfg} +map #{hlds_mapcycle.first} +fps_max #{fps_max}"}
+    set :hlds_game, "tf"
+    set :fps_max, "1000"
+    set :maxplayers, "24"
+    set :server_ip, "0.0.0.0"
+    set :server_port, "27015"
+    set :server_type_cfg, "server.cfg" # "server.cfg"
+    set :hlds_mapcycle, %w(ctf_2fort pl_hoodoo)
     set :hlds_motd, "welcome"
     set :hlds_config_hostname, "TF2 Server"
-    set :hlds_config_sv_contact, "Unset as of Yet"
+    set :hlds_config_sv_contact, "unset"
     set :hlds_config_sv_region, "1"  # -1 is the world, 0 is USA east coast, 1 is USA west coast 2 south america, 3 europe, 4 asia, 5 australia, 6 middle east, 7 africa
     set(:hlds_config_rcon_password) {utilities.ask("rcon_password")}
     set :hlds_config_sv_password, nil # connect password
@@ -66,6 +72,14 @@ Capistrano::Configuration.instance(true).load do
       put hlds_mapcycle.join("\n")+"\n", "/tmp/mapcycle.txt", :owner => "#{hlds_user}:#{hlds_user}"
       run "#{sudo} cp #{hlds_bindir}/tf/mapcycle.txt #{hlds_bindir}/tf/mapcycle.txt.`date +%s`"
       run "#{sudo} mv /tmp/mapcycle.txt #{hlds_bindir}/tf/mapcycle.txt"
+    end
+
+    task :setup_ugc, :roles => :hlds do
+      utilities.sudo_upload_template ugc_files, "#{hlds_config_root}/ugc.tar", :owner => "#{hlds_user}:#{hlds_user}", :mode => "755"
+      run "#{sudo} tar -xf #{hlds_config_root}/ugc.tar -C #{hlds_config_root}"
+      sudo "chown -R #{hlds_user}:#{hlds_user} #{hlds_config_root}"
+      sudo "rm #{hlds_config_root}/ugc.tar"
+
     end
 
     task :update, :roles => :hlds do
