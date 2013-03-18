0ce18162671f3a60aeb8f55e479e8e9d7fd095e8
Donovan Bray <donovan@homerun.com>
2013-03-18 11:36:25 -0700

    do a restart after a newrelic_symond:setup

--- lib/cap_recipes/tasks/newrelic_sysmond/hooks.rb
+++ lib/cap_recipes/tasks/newrelic_sysmond/hooks.rb
@@ -3,5 +3,6 @@
 Capistrano::Configuration.instance(true).load do
   after "deploy:provision", "newrelic_sysmond:install"
   after "newrelic_sysmond:install", "newrelic_sysmond:setup"
+  after "newrelic_sysmond:setup", "newrelic_symond:restart"
   on :load, "newrelic_sysmond:watcher"
 end
