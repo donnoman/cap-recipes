013d75efa65d90d5e4b2626e89fcfa2eb842098e
ascruse <scott.cruse@deem.com>
2013-03-26 12:28:46 -0700

    typo fixed for newrelic_sysmond:install

--- lib/cap_recipes/tasks/newrelic_sysmond/hooks.rb
+++ lib/cap_recipes/tasks/newrelic_sysmond/hooks.rb
@@ -3,6 +3,6 @@
 Capistrano::Configuration.instance(true).load do
   after "deploy:provision", "newrelic_sysmond:install"
   after "newrelic_sysmond:install", "newrelic_sysmond:setup"
-  after "newrelic_sysmond:setup", "newrelic_symond:restart"
+  after "newrelic_sysmond:setup", "newrelic_sysmond:restart"
   on :load, "newrelic_sysmond:watcher"
 end
