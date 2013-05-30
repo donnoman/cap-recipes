3af198227712d2e7fd061c6f0dcd95d687c20c9a
Rick Russell <rick.russell@lookout.com>
2013-05-30 16:33:33 -0700

    fix csgo init.d file and start commands

--- lib/cap_recipes/tasks/csgo_ds/install.rb
+++ lib/cap_recipes/tasks/csgo_ds/install.rb
@@ -12,17 +12,19 @@ Capistrano::Configuration.instance(true).load do
     set :steamcmd_wget_url, "http://media.steampowered.com/client/steamcmd_linux.tar.gz"
     set :csgo_ds_init_erb, File.join(File.dirname(__FILE__),'csgo_ds.init.erb')
     set :csgo_ds_init_erb_dest, '/etc/init.d/csgo_ds'
-    set :csgo_ds_config_erb, File.join(File.dirname(__FILE__),'GameModes_Server.txt.erb')
+    set :csgo_ds_config_erb, File.join(File.dirname(__FILE__),'server.cfg.erb')
+    set :csgo_ds_update_erb, File.join(File.dirname(__FILE__),'update_csgo.sh.erb')
     set :csgo_ds_motd_txt_erb, File.join(File.dirname(__FILE__),'motd.txt.erb')
     set(:csgo_ds_config_root) {"#{steamcmd_app_root}/cfg"}
     set(:csgo_ds_config_server_cfg) {"#{csgo_ds_config_root}/server.cfg"}
-    set(:csgo_ds_parameters) {"./srcds_run -game ${GAME_NAME} -console -usercon -ip ${GAME_IP} -port ${GAME_PORT} +fps_max ${GAME_FPS} +game_type ${GAME_TYPE} +game_mode ${GAME_MODE} +map ${GAME_MAP} -autoupdate -steam_dir ${STEAMCMD}"}
+set(:csgo_ds_parameters) {"./srcds_run -game ${GAME_NAME} -console -usercon -ip ${GAME_IP} -port ${GAME_PORT} +fps_max ${GAME_FPS} +game_type ${GAME_TYPE} +game_mode ${GAME_MODE} -maxplayers_override ${MAX_PLAYERS} +map ${GAME_MAP} -autoupdate -steamcmd_script ${STEAMCMD} -steam_dir ${STEAM_DIR}"}
     set :csgo_ds_game, "csgo"
     set :fps_max, "1000"
-    set :maxplayers, "24"
+    set :maxplayers, "16"
     set :server_ip, "0.0.0.0"
     set :server_port, "27015"
-    set :csgo_ds_mapcycle, %w(cs_italy de_dust de_aztec cs_office de_dust2 de_train de_inferno de_nuke)
+    set :csgo_ds_mapcycle_array, %w(cs_italy de_dust de_aztec cs_office de_dust2 de_train de_inferno de_nuke)
+    set :csgo_ds_mapcycle, "cs_italy de_dust de_aztec cs_office de_dust2 de_train de_inferno de_nuke"
     set :csgo_ds_motd, "Welcome to a CS:GO Dedicated Server!"
     set :csgo_ds_config_hostname, "CS:GO Dedicated Server"
     set :csgo_ds_config_sv_contact, "unset"
@@ -53,23 +55,24 @@ Capistrano::Configuration.instance(true).load do
       run "#{sudo} chown -R #{steamcmd_user}:#{steamcmd_user} #{steamcmd_app_root}"
     end
 
-    task :install_csgo, :roles => :csgo_ds do
+    task :install, :roles => :csgo_ds do
       run "cd #{steamcmd_app_root} && ./steamcmd.sh +login anonymous +force_install_dir #{csgo_ds_source} +app_update 740 validate +quit"
     end
 
-    task :update_csgo, :roles => :csgo_ds do
-      run "cd #{csgo_ds_source} && #{sudo} ./app_update 740"
-      run "#{sudo} chown -R #{steamcmd_user}:#{steamcmd_user} #{csgo_ds_source}"
+    task :update, :roles => :csgo_ds do
+      run "cd #{steamcmd_app_root} && ./steamcmd.sh +runscript #{csgo_ds_source}/update_csgo.sh"
     end
 
     task :setup, :roles => :csgo_ds do
       utilities.sudo_upload_template csgo_ds_init_erb, csgo_ds_init_erb_dest, :owner => "root:root", :mode => "700"
       utilities.sudo_upload_template csgo_ds_motd_txt_erb, "#{csgo_ds_source}/csgo/motd.txt", :owner => "#{steamcmd_user}:#{steamcmd_user}"
+      utilities.sudo_upload_template csgo_ds_config_erb, "#{csgo_ds_source}/csgo/cfg/server.cfg", :owner => "#{steamcmd_user}:#{steamcmd_user}", :mode => "755"
+      utilities.sudo_upload_template csgo_ds_update_erb, "#{csgo_ds_source}/update_csgo.sh", :owner => "#{steamcmd_user}:#{steamcmd_user}", :mode => "755"
       run "#{sudo} chown -R #{steamcmd_user}:#{steamcmd_user} #{csgo_ds_source}"
     end
 
     %w(start stop restart).each do |t|
-      task t.to_sym, :roles => [:csgo_ds, :csgo_ds_ugc] do
+      task t.to_sym, :roles => [:csgo_ds] do
         run "#{sudo} /etc/init.d/csgo_ds #{t}"
       end
     end
