9517319bfb05b06360a0285d26c75ba2e3bc4665
Danny Kirschner <rkirschner1377@gmail.com>
2013-06-25 17:37:11 -0300

    Update postgresql.rb

--- lib/cap_recipes/tasks/postgresql.rb
+++ lib/cap_recipes/tasks/postgresql.rb
@@ -13,14 +13,14 @@ require File.expand_path(File.dirname(__FILE__) + '/utilities')
 
 Capistrano::Configuration.instance(true).load do
   
-  set :postgresql_host, {"localhost"}
-  set :postgresql_user, {application}
-  set :postgresql_password, {Capistrano::CLI.password_prompt "PostgreSQL Password: "}
-  set :postgresql_database, {"#{application}_production"}
-  set :postgresql_dump_path, {"#{current_path}/tmp"}
-  set :postgresql_dump_file, {"#{application}_dump.sql"}
-  set :postgresql_local_dump_path, {File.expand_path("../../../tmp", __FILE__) }
-  set :postgresql_pid, {"/var/run/postgresql/9.2-main.pid"}
+  set :postgresql_host, "localhost"
+  set :postgresql_user, application
+  set(postgresql_password) {Capistrano::CLI.password_prompt "PostgreSQL Password: "}
+  set(postgresql_database) {"#{application}_production"}
+  set(postgresql_dump_path) {"#{current_path}/tmp"}
+  set(postgresql_dump_file( {"#{application}_dump.sql"}
+  set(postgresql_local_dump_path) {File.expand_path("../../../tmp", __FILE__) }
+  set(postgresql_pid) {"/var/run/postgresql/9.2-main.pid"}
   
   namespace :postgresql do
   desc "Install the latest stable release of PostgreSQL."
