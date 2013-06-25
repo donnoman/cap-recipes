174bb8d8d1ec709a93066f3d1461b43d23f22d9f
Danny Kirschner <rkirschner1377@gmail.com>
2013-06-25 17:38:07 -0300

    Update postgresql.rb

--- lib/cap_recipes/tasks/postgresql.rb
+++ lib/cap_recipes/tasks/postgresql.rb
@@ -18,7 +18,7 @@ Capistrano::Configuration.instance(true).load do
   set(postgresql_password) {Capistrano::CLI.password_prompt "PostgreSQL Password: "}
   set(postgresql_database) {"#{application}_production"}
   set(postgresql_dump_path) {"#{current_path}/tmp"}
-  set(postgresql_dump_file( {"#{application}_dump.sql"}
+  set(postgresql_dump_file) {"#{application}_dump.sql"}
   set(postgresql_local_dump_path) {File.expand_path("../../../tmp", __FILE__) }
   set(postgresql_pid) {"/var/run/postgresql/9.2-main.pid"}
   
