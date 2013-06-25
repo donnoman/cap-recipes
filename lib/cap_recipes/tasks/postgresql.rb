7014a04d079050877d4af9b508e51409a7e3d58e
Danny Kirschner <rkirschner1377@gmail.com>
2013-06-25 16:58:53 -0300

    changing hooks to work with deploy:setup

--- lib/cap_recipes/tasks/postgresql.rb
+++ lib/cap_recipes/tasks/postgresql.rb
@@ -14,21 +14,21 @@ namespace :postgresql do
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install postgresql libpq-dev"
   end
-  after "deploy:install", "postgresql:install"
+  after "postgresql:install", "postgresql:create_database"
 
   desc "Create a database for this application."
   task :create_database, roles: :db, only: {primary: true} do
     run %Q{#{sudo} -u postgres psql -c "create user #{postgresql_user} with password '#{postgresql_password}';"}
     run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner #{postgresql_user};"}
   end
-  after "deploy:setup", "postgresql:create_database"
+  after "deploy:setup", "postgresql:install"
 
   desc "Generate the database.yml configuration file."
   task :setup, roles: :app do
     run "mkdir -p #{shared_path}/config"
     template "postgresql.yml.erb", "#{shared_path}/config/database.yml"
   end
-  after "deploy:setup", "postgresql:setup"
+  after "postgresql:create_database", "postgresql:setup"
 
   desc "Symlink the database.yml file into latest release"
   task :symlink, roles: :app do
