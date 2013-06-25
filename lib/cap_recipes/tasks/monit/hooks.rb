47fb37e9615143ebc5f7ed3aa5ced17321e8a31c
Danny Kirschner <rkirschner1377@gmail.com>
2013-06-25 14:58:17 -0300

    adding hook after setup to install

--- lib/cap_recipes/tasks/monit/hooks.rb
+++ lib/cap_recipes/tasks/monit/hooks.rb
@@ -1,4 +1,5 @@
 Capistrano::Configuration.instance(true).load do
   after "deploy:provision", "monit:install"
+  after "deploy:setup", "monit:install"
   after "deploy:restart", "monit:enable", "monit:restart"
 end
