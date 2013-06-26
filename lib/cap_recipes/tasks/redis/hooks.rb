7d37e5130a3e6f903c4027e284a01d5b6d652cb8
Danny Kirschner <rkirschner1377@gmail.com>
2013-06-26 12:22:31 -0400

    relying on deploy:provision once more

--- lib/cap_recipes/tasks/redis/hooks.rb
+++ lib/cap_recipes/tasks/redis/hooks.rb
@@ -1,6 +1,5 @@
 Capistrano::Configuration.instance(true).load do
   after "deploy:provision", "redis:install"
-  after "deploy:setup", "redis:install"
   after "redis:install", "redis:setup"
   # after "redis:setup", "redis:cli_helper"
   after "redis:setup", "redis:logrotate"
