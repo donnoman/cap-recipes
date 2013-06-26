0498f4f34b043d4e039900ddadae6cd0eb93f376
Danny Kirschner <rkirschner1377@gmail.com>
2013-06-26 12:24:08 -0400

    removing deploy:setup install hook

--- lib/cap_recipes/tasks/monit/hooks.rb
+++ lib/cap_recipes/tasks/monit/hooks.rb
@@ -1,5 +1,4 @@
 Capistrano::Configuration.instance(true).load do
   after "deploy:provision", "monit:install"
-  after "deploy:setup", "monit:install"
   after "deploy:restart", "monit:enable", "monit:restart"
 end
