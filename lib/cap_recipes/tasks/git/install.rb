74c29a2455d74990450700e8f99a7579043d7dca
Donovan Bray <donovan@homerun.com>
2013-07-02 16:36:17 -0700

    added task to install git from the git-core PPA

---
+++ lib/cap_recipes/tasks/git/install.rb
@@ -0,0 +1,21 @@
+require File.expand_path(File.dirname(__FILE__) + '/../utilities')
+
+Capistrano::Configuration.instance(true).load do
+
+  namespace :git do
+
+    desc "install git"
+    task :install, :except => {:no_release => true} do
+      utilities.apt_install "python-software-properties"
+      sudo "sudo add-apt-repository ppa:git-core/ppa"
+      utilities.apt_update
+      utilities.apt_install "git-core"
+    end
+
+    desc "git version"
+    task :version, :except => {:no_release => true} do
+      run "git --version"
+    end
+
+  end
+end
