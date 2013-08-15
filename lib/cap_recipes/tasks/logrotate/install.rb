b2b9c25211b940c47a8c2ebc339e1b2e5d3dd71a
Donovan Bray <donovan@homerun.com>
2013-08-15 06:56:04 -0700

    allow an option to symlink the log directories somewhere else

--- lib/cap_recipes/tasks/logrotate/install.rb
+++ lib/cap_recipes/tasks/logrotate/install.rb
@@ -5,18 +5,48 @@ Capistrano::Configuration.instance(true).load do
     set(:logrotate_path) { shared_path }
     set :logrotate_dir, 'log'
     set :logrotate_keep_logs, 10
+    set :logrotate_symlink_to_path, nil # like ephemeral volumes /mnt/logs/#{application}
 
     def log_dir(num)
       File.join(logrotate_path, num == 0 ? logrotate_dir : "#{logrotate_dir}.#{num.to_s}")
     end
 
+    def symlink_dir(num)
+      File.join(logrotate_symlink_to_path, num == 0 ? logrotate_dir : "#{logrotate_dir}.#{num.to_s}")
+    end
+
+    def make_log_dir(num)
+      if logrotate_symlink_to_path
+        # TODO if log_dir not a symlink move it before doing anything
+        run "mkdir -p #{symlink_dir(num)}"
+        run "ln -sf #{symlink_dir(num)} #{log_dir(num)}"
+        run "mv #{symlink_dir(num)} #{symlink_dir(num+1)}" unless num == 0
+      else
+        run "mkdir -p #{log_dir(num)}"
+        run "mv #{log_dir(num)} #{log_dir(num+1)}" unless num == 0
+      end
+    end
+
+    def del_log_dir(num)
+      if logrotate_symlink_to_path
+        run "rm #{log_dir(num)}"
+        run "rm -rf #{symlink_dir(num)}"
+      else 
+        run "rm -rf #{log_dir(num)}"
+      end
+    end
+
     desc "rotate the log directory"
     task :rotate, :except => { :no_release => true } do
-      (0..logrotate_keep_logs).to_a.reverse.each do |num|
-        run "mkdir -p #{log_dir(num)}; mv #{log_dir(num)} #{log_dir(num+1)}"
+      run "mkdir -p #{logrotate_path}"
+      if logrotate_symlink_to_path
+        run "#{sudo} mkdir -p #{logrotate_symlink_to_path}; chown -R #{user}:#{user} #{logrotate_symlink_to_path}"
       end
-      run "mkdir -p #{log_dir(0)}"
-      run "rm -rf #{log_dir(logrotate_keep_logs+1)}"
+      (0..logrotate_keep_logs).to_a.reverse.each do |num|
+        make_log_dir(num)
+      end
+      make_log_dir(0)
+      del_log_dir(logrotate_keep_logs+1)
     end
 
   end
