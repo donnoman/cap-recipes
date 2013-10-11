7512020872d86134a07f436ebebff2d01569ea64
Dennis Faust <dennis@dennisfaust.com>
2013-10-11 16:22:53 -0700

    Create a notice_deployment task for NewRelic.

--- lib/cap_recipes/tasks/newrelic_rpm/hooks.rb
+++ lib/cap_recipes/tasks/newrelic_rpm/hooks.rb
@@ -1,4 +1,5 @@
 # @author Donovan Bray <donnoman@donovanbray.com>
 
 Capistrano::Configuration.instance(true).load do
+    after "deploy:provision", "newrelic_rpm:notice_deployment"
 end
--- lib/cap_recipes/tasks/newrelic_rpm/install.rb
+++ lib/cap_recipes/tasks/newrelic_rpm/install.rb
@@ -5,5 +5,13 @@ Capistrano::Configuration.instance(true).load do
   namespace :newrelic_rpm do
     roles[:newrelic_rpm]
     set(:newrelic_rpm_license_key) {utilities.ask("newrelic_rpm_license_key:")}
+
+
+    desc "Notify New Relic so the deploys will appear in the time series graphs."
+    task :notice_deployment do
+      # https://docs.newrelic.com/docs/ruby/recording-deployments-with-the-ruby-agent#Manual
+      newrelic deployments 
+    end
+
   end
 end
