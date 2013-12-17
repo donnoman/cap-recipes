dd817c5f2819e7a03f2ed14bca7d9a26d0029a61
Donovan Bray <donovan@homerun.com>
2013-12-17 12:22:55 -0800

    Add task to newrelic_rpm to use the cli to send a deployment notice including a change log to NewRelic RPM.

--- lib/cap_recipes/tasks/newrelic_rpm/hooks.rb
+++ lib/cap_recipes/tasks/newrelic_rpm/hooks.rb
@@ -1,5 +1,6 @@
 # @author Donovan Bray <donnoman@donovanbray.com>
 
 Capistrano::Configuration.instance(true).load do
-    after "deploy:provision", "newrelic_rpm:notice_deployment"
+  after "deploy:restart", "newrelic_rpm:notice_deployment"
+  after "deploy:start", "newrelic_rpm:notice_deployment"
 end
--- lib/cap_recipes/tasks/newrelic_rpm/install.rb
+++ lib/cap_recipes/tasks/newrelic_rpm/install.rb
@@ -3,14 +3,25 @@ require File.expand_path(File.dirname(__FILE__) + '/../utilities')
 
 Capistrano::Configuration.instance(true).load do
   namespace :newrelic_rpm do
-    roles[:newrelic_rpm]
+    #this variable is for compatability with newrelic_nrsysmond, it's not used directly here.
     set(:newrelic_rpm_license_key) {utilities.ask("newrelic_rpm_license_key:")}
-
+    set(:newrelic_rpm_cli) { "bundle exec newrelic" }
+    set(:newrelic_rpm_source_benchmark_revision) { previous_revision }
+    set(:newrelic_rpm_source_changes_command) {
+      case scm
+      when :git
+        %Q{#{source.command} --no-pager log #{newrelic_rpm_source_benchmark_revision}..#{latest_revision} --no-color --pretty=format:"%s - %an, %ad : %H"}
+      else
+        %Q{echo "no changes command implemented."}
+      end
+    }
 
     desc "Notify New Relic so the deploys will appear in the time series graphs."
-    task :notice_deployment do
+    task :notice_deployment, :except => { :no_release => true }, :once => true do
       # https://docs.newrelic.com/docs/ruby/recording-deployments-with-the-ruby-agent#Manual
-      newrelic deployments 
+      # The return of this command is coerced to true because we don't want any failure in
+      # noticing a deployment to break the deploy.
+      run "cd #{latest_release} && #{newrelic_rpm_source_changes_command} | #{newrelic_rpm_cli} deployments --environment=#{rails_env} --revision=#{latest_revision} --changes; true"
     end
 
   end
