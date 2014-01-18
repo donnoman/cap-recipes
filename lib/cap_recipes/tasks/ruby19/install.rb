072ed2b11a6bd7486ddacc49d7817b3db974a509
Donovan Bray <donovan@homerun.com>
2014-01-18 12:00:04 -0800

    separate ruby_debugger support to make it independently testable, add rubygems_source_fix for dns resolution issues of rubygems.org

--- lib/cap_recipes/tasks/ruby19/install.rb
+++ lib/cap_recipes/tasks/ruby19/install.rb
@@ -9,6 +9,7 @@ Capistrano::Configuration.instance(true).load do
     set(:ruby_src){"ftp://ftp.ruby-lang.org/pub/ruby/1.9/#{ruby_ver}.tar.bz2"}
     set :base_ruby_path, '/usr'
     set :ruby_debugger_support, true
+    set :rubygems_source_fix_support, true
 
     # New Concept ':except => {:no_ruby => true}' to allow all systems by default
     # to have ruby installed to allow use of ruby gems like god on all systems
@@ -22,9 +23,21 @@ Capistrano::Configuration.instance(true).load do
       run "#{sudo} rm -rf /usr/local/src/#{ruby_ver}" #make clean is not allowing a re-install  #http://www.ruby-forum.com/topic/4409005
       run "cd /usr/local/src && #{sudo} wget --tries=2 -c --progress=bar:force #{ruby_src} && #{sudo} bunzip2 --keep --force #{ruby_ver}.tar.bz2 && #{sudo} tar xvf #{ruby_ver}.tar"
       run "cd /usr/local/src/#{ruby_ver} && #{sudo} ./configure --prefix=#{base_ruby_path} --enable-shared && #{sudo} make install"
+    end
+
+    desc "add ruby debugger support"
+    task :ruby_debugger, :except => { :no_ruby => true } do
       if ruby_debugger_support
         run "#{sudo} gem install debugger-ruby_core_source --no-rdoc --no-ri -- --with-ruby-include=/usr/local/src/#{ruby_ver}"
         run "#{sudo} gem install debugger-linecache --no-rdoc --no-ri -- --with-ruby-include=/usr/local/src/#{ruby_ver}"
+      end
+    end
+
+    desc "Remove legacy rubygems.org as gem source"
+    task :rubygems_source_fix, :except => { :no_ruby => true } do
+      if rubygems_source_fix_support
+        run "#{sudo} gem source -a http://production.s3.rubygems.org"
+        run "#{sudo} gem source -r http://rubygems.org/"
       end
     end
 
