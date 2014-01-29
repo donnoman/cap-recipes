3d86f7807db94d3b9a42c39da128d057084e4bdd
Donovan Bray <donovan@homerun.com>
2014-01-29 13:53:26 -0800

    order is important but ruby shouldn't be run before deploy:provision

--- lib/cap_recipes/tasks/ruby19/hooks.rb
+++ lib/cap_recipes/tasks/ruby19/hooks.rb
@@ -1,5 +1,5 @@
 Capistrano::Configuration.instance(true).load do
-  before "deploy:provision", "ruby19:install"
+  after "deploy:provision", "ruby19:install"
   after "ruby19:install", "ruby19:rubygems_source_fix"
   after "ruby19:install", "ruby19:ruby_debugger"
 end
