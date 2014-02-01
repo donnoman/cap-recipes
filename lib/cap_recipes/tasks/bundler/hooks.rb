99211f01a57d6ed80f1ae700e49afa5e1ba58047
Donovan Bray <donovan@homerun.com>
2014-01-31 17:24:08 -0800

    the rails assets hook executes before bundler which messes things up, this binds bundler:configure so that it runs before the call to assets.

--- lib/cap_recipes/tasks/bundler/hooks.rb
+++ lib/cap_recipes/tasks/bundler/hooks.rb
@@ -1,7 +1,7 @@
 # @author Donovan Bray <donnoman@donovanbray.com>
 Capistrano::Configuration.instance(true).load do
   before "bundler:configure", "bundler:install"
-  after "deploy:update_code", "bundler:configure"
+  before "deploy:finalize_update", "bundler:configure"
   after "deploy:provision", "bundler:install"
   after "deploy:restart", "bundler:save_bundle"
 end
