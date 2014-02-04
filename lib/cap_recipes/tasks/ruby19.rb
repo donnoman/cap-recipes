a996a45485c233ae99deb83166a2ceda5516ebe6
Donovan Bray <donovan@homerun.com>
2014-02-04 10:43:07 -0800

    deprecate ruby19 recipe, and modify the ruby recipe to be version agnostic.

--- lib/cap_recipes/tasks/ruby19.rb
+++ lib/cap_recipes/tasks/ruby19.rb
@@ -1,1 +1,2 @@
-Dir[File.join(File.dirname(__FILE__), 'ruby19/*.rb')].sort.each { |lib| require lib }
+puts "WARNING: ruby19 has been removed require 'cap_recipes/tasks/ruby' instead."
+Dir[File.join(File.dirname(__FILE__), 'ruby/*.rb')].sort.each { |lib| require lib }
