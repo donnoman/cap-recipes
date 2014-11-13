d5484c1b8a3e57f39f1607438f066b1850dfd87f
Donovan Bray <donovan@homerun.com>
2014-11-13 13:56:24 -0800

    duplicate reference removed

--- lib/cap_recipes/tasks/mysql_master/install.rb
+++ lib/cap_recipes/tasks/mysql_master/install.rb
@@ -10,7 +10,6 @@ Capistrano::Configuration.instance(true).load do
     set :mysql_master_increment_id, nil
     set :mysql_master_offset_id, nil
     set :mysql_slave_server_id, nil
-    set :mysql_repl_pass, nil
     set :mysql_listen_interface, "eth0"
     set :mysql_data_dir, "/var/lib/mysql"
     set :mysql_log_dir, "/var/log/mysql"
